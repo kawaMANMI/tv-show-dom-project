7701c71b1ba5f949d813226e204011806f91aee2
Kawa MANMI <kawamam2007@gmail.com>
2022-10-10 23:58:24 +0100

    Level 200 finished, think so

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
 //You can edit ALL of the code here
+var allEpisodes = getAllEpisodes();
 function setup() {
-  const allEpisodes = getAllEpisodes();
-  console.log(allEpisodes.id);
   makePageForEpisodes(allEpisodes);
 }
-
-var headerDiv = document.createElement("div");
-headerDiv.className="headerInfo"
-document.body.prepend(headerDiv);
+//Conatiner header start
+var containerHeader = document.createElement("div");
+containerHeader.className = "containerHeader";
+document.body.prepend(containerHeader);
 
 var h1 = document.createElement("h1");
 h1.innerHTML = "Info about all episodes are as follows";
-headerDiv.appendChild(h1);
+containerHeader.appendChild(h1);
 var SourceInfo = document.createElement("p");
 SourceInfo.innerHTML = "the data has (originally) come from ";
 SourceInfo.className = "sourceInfo";
-headerDiv.appendChild(SourceInfo);
+containerHeader.appendChild(SourceInfo);
 var linkSource = document.createElement("a");
 var linkText = document.createTextNode("TVMaze.com");
 linkSource.appendChild(linkText);
@@ -23,9 +22,12 @@ linkSource.title = "";
 linkSource.href = "https://www.tvmaze.com/";
 SourceInfo.appendChild(linkSource);
 
+// container header end
+
 const rootElem = document.getElementById("root");
 
 function makePageForEpisodes(episodeList) {
+  console.log("Kawa");
   episodeList.forEach((episodeListElm) => {
     let containerEpisode = document.createElement("div");
     containerEpisode.className = "containerEpisode";
@@ -38,6 +40,7 @@ function makePageForEpisodes(episodeList) {
     nameEpisode.innerHTML = `${episodeListElm.name} - ${
       seasonNumber + episodeNumber
     }`;
+    // episodeListElm[seasonAndEpisodeNumber] = seasonNumber + episodeNumber;
     let image = document.createElement("img");
     containerEpisode.appendChild(image);
     image.className = "image";
@@ -52,5 +55,31 @@ function makePageForEpisodes(episodeList) {
   });
 }
 
-window.onload = setup;
+const input = document.createElement("input");
+input.setAttribute("type", "search");
+input.setAttribute("autocomplete", "off");
+input.setAttribute("placeholder", "Search for your episod");
+input.setAttribute("id", "inputSearch");
+containerHeader.appendChild(input);
+const labelInputSearch = document.createElement("label");
+labelInputSearch.setAttribute("for", inputSearch);
+// labelInputSearch.innerHTML = `  Number of found episodes is ${episodeMatch.length} `;
+labelInputSearch.className = containerHeader.appendChild(labelInputSearch);
 
+input.addEventListener("keyup", updateValue);
+
+let episodeMatch = "";
+function updateValue(e) {
+  let realTimeInputValue = e.target.value.toLowerCase();
+  rootElem.innerHTML = "";
+  episodeMatch = allEpisodes.filter(
+    (episodeElm) =>
+      episodeElm.name.toLowerCase().includes(realTimeInputValue) |
+      episodeElm.summary.toLowerCase().includes(realTimeInputValue)
+  );
+  console.log(episodeMatch);
+  makePageForEpisodes(episodeMatch);
+  labelInputSearch.innerHTML = `  Display ${episodeMatch.length} / ${allEpisodes.length}  episodes`;
+}
+
+window.onload = setup;

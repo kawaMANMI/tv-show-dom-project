fba0d4b98fa81f825674f786263d3a4c1f2544b1
Kawa MANMI <kawamam2007@gmail.com>
2022-10-10 01:07:38 +0100

    The title of each episod shown + the symbol of each episod by addeing season number and episod number

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 //You can edit ALL of the code here
 function setup() {
   const allEpisodes = getAllEpisodes();
+  console.log(allEpisodes.id);
   makePageForEpisodes(allEpisodes);
 }
+var h1 = document.createElement("h1");
+h1.innerHTML = "The list of the episodes are as follows";
+document.body.prepend(h1);
 
+const rootElem = document.getElementById("root");
 function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  episodeList.forEach((episodeListElm) => {
+    let containerEpisode = document.createElement("div");
+    rootElem.appendChild(containerEpisode);
+    let nameEpisode = document.createElement("p");
+    nameEpisode.className = nameEpisode;
+    containerEpisode.appendChild(nameEpisode);
+    let seasonNumber = "S" + episodeListElm.season.toString().padStart(2, "0");
+    let episodeNumber = "E" + episodeListElm.number.toString().padStart(2, "0");
+    nameEpisode.innerHTML = `${episodeListElm.name} - ${
+      seasonNumber + episodeNumber
+    }`;
+  });
 }
 
 window.onload = setup;

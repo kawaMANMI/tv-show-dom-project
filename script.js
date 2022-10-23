d5ce16e87c67503d2f64b8575f6178d608d429c6
Kawa MANMI <kawamam2007@gmail.com>
2022-10-23 16:31:24 +0100

    Level 400 has been done

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
 // Creating container of header of the page
-
-console.log(getAllShows());
-
 var containerHeader = document.createElement("div");
 containerHeader.className = "containerHeader";
 document.body.prepend(containerHeader);
@@ -23,27 +20,45 @@ SourceInfo.appendChild(linkSource);
 
 // Root element is here rootElem
 const rootElem = document.getElementById("root");
+const labelAllShows = document.createElement("label");
+labelAllShows.innerHTML = "List of All Shows ";
+labelAllShows.className = "labelInputSearch";
+containerHeader.appendChild(labelAllShows);
+const selectAllShows = document.createElement("select");
+containerHeader.appendChild(selectAllShows);
+selectAllShows.name = "listOfAllShows";
+selectAllShows.id = "listOfAllShows";
+const allShows = getAllShows();
+dropDownListAllShows(allShows);
 
-//Get Data using APIs
-var url = "https://api.tvmaze.com/shows/82/episodes";
-fetch(url)
-  .then((response) => {
-    return response.json();
-  })
-  .then((allEpisodes) => {
-    // console.log(allEpisodes);
-    function setup(allEpisodes) {
-      dropDownListOfEpisods(allEpisodes);
-      liveSearch(allEpisodes);
-      makePageForEpisodes(allEpisodes);
-    }
+//Getting Data using APIs
+var url;
+function loadData() {
+  fetch(url)
+    .then((response) => {
+      // alert("Some data loaded in Fetch");
+      return response.json();
+    })
+    .then((allEpisodes) => {
+      // console.log(allEpisodes);
+      function setup(allEpisodes) {
+        dropDownListOfEpisodes(allEpisodes);
+        liveSearch(allEpisodes);
+        makePageForEpisodes(allEpisodes);
+        // alert("Some data loaded in Fetch");
+      }
 
-    setup(allEpisodes);
-  });
+      setup(allEpisodes);
+    })
+    .catch((error) => {
+      alert("The data can't be loaded  " + error);
+    });
+}
 //End of Getting Data
 
 //Function  to make page for all episodes
 function makePageForEpisodes(episodeList) {
+  rootElem.innerHTML = "";
   episodeList.forEach((episodeListElm) => {
     let containerEpisode = document.createElement("div");
     containerEpisode.className = "containerEpisode";
@@ -74,17 +89,21 @@ function makePageForEpisodes(episodeList) {
 
 //DropDown list function
 //Dropdown List part start from here
-function dropDownListOfEpisods(allEpisodes) {
-  var select = document.createElement("select");
-  containerHeader.appendChild(select);
-  select.name = "listOfEpisodes";
-  select.id = "listOfEpisodes";
+const labelListOfEpisodes = document.createElement("label");
+labelListOfEpisodes.innerHTML = "List of all episodes ";
+labelListOfEpisodes.className = "labelListOfEpisodes";
+containerHeader.appendChild(labelListOfEpisodes);
+const selectEpisodes = document.createElement("select");
+containerHeader.appendChild(selectEpisodes);
+selectEpisodes.name = "listOfEpisodes";
+selectEpisodes.id = "listOfEpisodes";
+function dropDownListOfEpisodes(allEpisodes) {
+  selectEpisodes.innerHTML = "";
   let option = document.createElement("option");
   option.value = -1;
   option.text = "Show All";
   // console.log(episodeElm.seasonAndEpisodeNumber);
-  select.appendChild(option);
-
+  selectEpisodes.appendChild(option);
   let countDropdownList = 0;
   for (const episodeListElm of allEpisodes) {
     let option = document.createElement("option");
@@ -94,37 +113,38 @@ function dropDownListOfEpisods(allEpisodes) {
     countDropdownList++;
     option.text = `${seasonNumber + episodeNumber} - ${episodeListElm.name} `;
     // console.log(episodeElm.seasonAndEpisodeNumber);
-    select.appendChild(option);
+    selectEpisodes.appendChild(option);
   }
+  labelInputSearch.innerHTML = `  Display ${allEpisodes.length} / ${allEpisodes.length}  Episodes`;
 
-  select.addEventListener("change", (event) => {
-    if (select.value > 0) {
-      episodeMatch = [allEpisodes[select.value]];
+  selectEpisodes.addEventListener("change", (event) => {
+    if (selectEpisodes.value >= 0) {
+      episodeMatch = [allEpisodes[selectEpisodes.value]];
     } else {
       episodeMatch = allEpisodes;
     }
-    rootElem.innerHTML = "";
+    input.value = "";
+    labelInputSearch.innerHTML = `  Display ${episodeMatch.length} / ${allEpisodes.length}  Episodes`;
     makePageForEpisodes(episodeMatch);
+    console.log("episodeMatch");
   });
 }
 //End dropdown list
 
+const input = document.createElement("input");
+input.setAttribute("type", "search");
+input.setAttribute("autocomplete", "off");
+input.setAttribute("placeholder", "Search for your episod");
+input.setAttribute("id", "inputSearch");
+containerHeader.appendChild(input);
+const labelInputSearch = document.createElement("label");
+labelInputSearch.setAttribute("for", inputSearch);
+// labelInputSearch.innerHTML = `  Display ${episodeMatch.length} / ${allEpisodes.length}  Episodes`;
+labelInputSearch.className = "labelInputSearch";
+containerHeader.appendChild(labelInputSearch);
 function liveSearch(allEpisodes) {
   //This part for live search
-  const input = document.createElement("input");
-  input.setAttribute("type", "search");
-  input.setAttribute("autocomplete", "off");
-  input.setAttribute("placeholder", "Search for your episod");
-  input.setAttribute("id", "inputSearch");
-  containerHeader.appendChild(input);
-  const labelInputSearch = document.createElement("label");
-  labelInputSearch.setAttribute("for", inputSearch);
-  labelInputSearch.innerHTML = "";
-  labelInputSearch.className = "labelInputSearch";
-  containerHeader.appendChild(labelInputSearch);
-
   input.addEventListener("keyup", updateValue);
-
   let episodeMatch = "";
   function updateValue(e) {
     let realTimeInputValue = e.target.value.toLowerCase();
@@ -140,3 +160,33 @@ function liveSearch(allEpisodes) {
   }
   // End live search part
 }
+
+//DropDown list function
+//Dropdown List All shows
+
+function dropDownListAllShows(allShows) {
+  let countDropdownList = 0;
+  for (const showsElm of allShows) {
+    let option = document.createElement("option");
+    option.value = countDropdownList;
+    if (countDropdownList === 0) {
+      option.selected = true;
+    }
+    countDropdownList++;
+    option.text = `${showsElm.name} `;
+    // console.log(episodeElm.seasonAndEpisodeNumber);
+    selectAllShows.appendChild(option);
+  }
+  url = `https://api.tvmaze.com/shows/${
+    allShows[selectAllShows.value].id
+  }/episodes`;
+  console.log(url);
+  loadData();
+  selectAllShows.addEventListener("change", (event) => {
+    url = `https://api.tvmaze.com/shows/${
+      allShows[selectAllShows.value].id
+    }/episodes`;
+    loadData();
+  });
+}
+//End dropdown list
